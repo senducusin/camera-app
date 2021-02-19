4480462913d31f7ef206bbe534c76b8e69c68ab2
Jansen Ducusin <senducusin@gmail.com>
2021-02-19 09:49:35 +0800

    Added permission request to access Photo library

--- Camera App/Controllers/PhotoListCollectionViewController.swift
+++ Camera App/Controllers/PhotoListCollectionViewController.swift
@@ -7,7 +7,30 @@
 
 import Foundation
 import UIKit
+import Photos
 
 class PhotoListCollectionViewController: UICollectionViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.populatePhotos()
+    }
+    
+    private func requestPermission(completion:@escaping(PHAuthorizationStatus) -> ()){
+        PHPhotoLibrary.requestAuthorization{ status in
+            DispatchQueue.main.async {
+                completion(status)
+            }
+        }
+    }
+    
+    private func populatePhotos(){
+        self.requestPermission(){ status in
+            if status == .authorized {
+                print("Authorized!")
+            }
+        }
+    }
     
 }
