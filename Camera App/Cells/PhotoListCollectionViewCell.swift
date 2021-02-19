2c4b8f6b2ffe81bc938e229b18bf08c6d339e326
Jansen Ducusin <senducusin@gmail.com>
2021-02-19 10:16:30 +0800

    Created a custom cellview controller

---
+++ Camera App/Cells/PhotoListCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoListCollectionViewCell.swift
+//  Camera App
+//
+//  Created by Jansen Ducusin on 2/19/21.
+//
+
+import Foundation
+import UIKit
+
+class PhotoListCollectionViewCell:UICollectionViewCell{
+    
+    @IBOutlet weak var photoImage:UIImageView!
+    
+}
