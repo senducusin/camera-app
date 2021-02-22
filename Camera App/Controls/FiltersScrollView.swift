7164b5d23affed45cdbae21c1b63f079b2c1afe3
Jansen Ducusin <senducusin@gmail.com>
2021-02-22 10:57:45 +0800

    Created a custom controller for UIScrollView that generates the scrollView's content programmatically

---
+++ Camera App/Controls/FiltersScrollView.swift
@@ -0,0 +1,54 @@
+//
+//  FiltersScrollView.swift
+//  Camera App
+//
+//  Created by Jansen Ducusin on 2/22/21.
+//
+
+import Foundation
+import UIKit
+import CoreImage
+
+class FiltersScrollView: UIScrollView, UIScrollViewDelegate{
+    
+    private var filterService: FilterService!
+    
+    init(parentView: UIView, frame: CGRect = CGRect.zero){
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit(){
+        delegate = self
+        self.filterService = FilterService()
+        setupFilters()
+    }
+    
+    private func setupFilters(){
+        var offset: CGFloat = 10.0
+        
+        for (index, filter) in FilterService.all().enumerated(){
+            
+            let filterImageView = UIImageView.imageForFilterView()
+            self.addSubview(filterImageView)
+            
+            filterImageView.tag = index
+            filterImageView.frame.origin.x = offset
+            filterImageView.center.y = self.frame.height/2 - 15
+            
+            offset += filterImageView.frame.width + filterImageView.frame.width/4
+            self.contentSize = CGSize(width: offset, height: self.frame.height)
+        }
+    }
+}
+
+extension FiltersScrollView {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        scrollView.contentOffset.y = 0
+    }
+}
--- Camera App/Extensions/UIImageView+Extensions.swift
+++ Camera App/Extensions/UIImageView+Extensions.swift
@@ -11,7 +11,7 @@ import UIKit
 extension UIImageView{
     static func imageForFilterView() -> UIImageView {
         let filterImageView = UIImageView(image: UIImage(named: "default-preview"))
-        filterImageView.frame = CGRect(origin: CGPoint.zero, size: CGSize(width: 80, height: 80))
+        filterImageView.frame = CGRect(origin: CGPoint.zero, size: CGSize(width: 70, height: 70))
         filterImageView.layer.cornerRadius = 6.0
         filterImageView.layer.masksToBounds = true
         return filterImageView
