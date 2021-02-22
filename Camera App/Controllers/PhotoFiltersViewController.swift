a687faf4d11379ab6a07d85e43e991d9b86d6c3b
Jansen Ducusin <senducusin@gmail.com>
2021-02-22 11:29:01 +0800

    Display captured image in PhotoFilterVC

--- Camera App/Controllers/PhotoFiltersViewController.swift
+++ Camera App/Controllers/PhotoFiltersViewController.swift
@@ -9,5 +9,18 @@ import Foundation
 import UIKit
 
 class PhotoFiltersViewController: UIViewController {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     var image: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    private func setupUI(){
+        self.photoImageView.image = self.image
+    }
 }
